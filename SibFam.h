4dcdc6f0d097ab5b080548209b63676548708530
fire-eggs <lifeattickville@gmail.com>
2023-02-18 21:47:46 -0500

    Start using Marriage class in SibFam.

--- SibFam.h
+++ SibFam.h
@@ -1,10 +1,10 @@
 #pragma once
 
-//#include <FL/Fl_Group.H>
 #include <FL/Fl_Multiline_Output.H>
 #include <FL/Fl_Box.H>
 
 #include "Person.h"
+#include "Marriage.h"
 
 // TODO can this and UncleFam be generalized?
 // TODO more than 3 children
@@ -12,21 +12,27 @@
 class SibFam : public Fl_Box
 {
 private:
-    Person *_husb;
-    Person *_wife;
+//    Person *_husb;
+//    Person *_wife;
     
     Fl_Color _clr;
     
-    Fl_Multiline_Output *dispHusb;
-    Fl_Multiline_Output *dispWife;
-    Fl_Box *dispPl;
+//    Fl_Multiline_Output *dispHusb;
+//    Fl_Multiline_Output *dispWife;
+//    Fl_Box *dispPl;
     
     int childCount;
-    Person *_childs[3];
-    Person *_childSpo[3];
+//    Person *_childs[3];
+//    Person *_childSpo[3];
 
-    int _marriageHigh; // height of husb+wife displays
+//    int _marriageHigh; // height of husb+wife displays
+
+    Marriage *_parents;
+    Marriage *children[3];
     
+    int _realW;
+    int _realH;
+       
 public:
     static const int FAM_WIDTH = 140;
     
@@ -36,9 +42,12 @@ public:
         box(FL_BORDER_BOX);
         
         _clr = clr;
-        _husb = husb;
-        _wife = wife;
-        
+//        _husb = husb;
+//        _wife = wife;
+
+        _parents = new Marriage(husb, wife, clr, x+3, y+3);
+
+#if 0        
         int husbH = _husb->high() + 5;
         int wifeH = _wife ? _wife->high() + 5 : 0;
         
@@ -54,16 +63,28 @@ public:
             _wife->setup(dispWife);
         }        
         //end();
-        
+#endif        
         childCount = 0;
 
+        _realW = _parents->realW()+6;
+        _realH = _parents->realH()+6;
         // Adjust our size for the required display dimensions
-        _marriageHigh = husbH + wifeH + 10;
-        resize(x,y,FAM_WIDTH, _marriageHigh);
+        resize(x,y, _realW, _realH);
     }
 
     void AddChild(Person *child, Person *spouse)
     {
+        int _marriageHigh = _parents->realH();
+
+        int deltaY = _marriageHigh + 3;
+        for (int i=0; i < childCount; i++)
+            deltaY += children[i]->realH() + 3;
+        
+        Marriage *childM = new Marriage(child, spouse, _clr, x()+22, y()+deltaY);
+        children[childCount] = childM;
+
+#if 0
+        
         // TODO heights should be calculated from Person
         
         _childs[childCount] = child;
@@ -89,8 +110,15 @@ public:
             spouse->setup(dispSpouse);
         else
             dispSpouse->clear_visible_focus(); // TODO fixme
-        
+#endif
+
+        _realW = Marriage::max(_realW, children[childCount]->realW()+25);
+        // TODO accumulated height; maximum width across parents, children
         resize(x(),y(),FAM_WIDTH, _marriageHigh + childCount * 77);
+        
+        childCount++;
     }
-    
+
+    int realW() { return _realW; }
+    int realH() { return _realH; }
 };
